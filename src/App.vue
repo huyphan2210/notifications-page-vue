8d5d3917bf0bb21d93678ef885da97ac5cf61906
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-06 08:01:46 +0700

    Remove the default

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  Notifications 3
+
+Mark all as read
+
+Mark Webber reacted to your recent post My first tournament today!
+1m ago
+
+Angela Gray followed you
+5m ago
+
+Jacob Thompson has joined your group Chess Club
+1 day ago
+
+Rizky Hasanuddin sent you a private message
+5 days ago
+Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+I'm already having lots of fun and improving my game.
+
+Kimberly Smith commented on your picture
+1 week ago
+
+Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
+2 weeks ago
+
+Anna Kim left the group Chess Club
+2 weeks ago
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
