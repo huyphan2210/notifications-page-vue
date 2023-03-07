05ef343f43e644cf22a8837b7f8b2d180520846b
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-07 09:48:24 +0700

    Host images

--- src/data/senders.ts
+++ src/data/senders.ts
@@ -2,46 +2,46 @@ import { Sender } from "../interfaces/Sender"
 
 const senders: Sender[] = [{
         senderName: 'Mark Webber',
-        avatarURL: 'src/assets/images/avatar-mark-webber.webp',
+        avatarURL: 'https://i.ibb.co/FXnGwHL/avatar-mark-webber.webp',
         action: 'reacted to your recent post',
         objectName: 'My First Tournament today!',
         time: '1m ago',
     }, {
         senderName: 'Angela Gray',
-        avatarURL: 'src/assets/images/avatar-angela-gray.webp',
+        avatarURL: 'https://i.ibb.co/gmfNhGg/avatar-angela-gray.webp',
         action: 'followed you',
         time: '5m ago'
     }, {
         senderName: 'Jacob Thompson',
-        avatarURL: 'src/assets/images/avatar-jacob-thompson.webp',
+        avatarURL: 'https://i.ibb.co/Z81dkKd/avatar-jacob-thompson.webp',
         action: 'has joined your group',
         objectName: 'Chest Club',
         time: '1 day ago',
         objectColor: 'hsl(219, 85%, 26%)'
     }, {
         senderName: 'Rizky Hasanuddin',
-        avatarURL: 'src/assets/images/avatar-rizky-hasanuddin.webp',
+        avatarURL: 'https://i.ibb.co/xDrMXDL/avatar-rizky-hasanuddin.webp',
         action: 'sent you a private message',
         time: '5 days ago',
         message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
         isRead: true
     }, {
         senderName: 'Kimberly Smith',
-        avatarURL: 'src/assets/images/avatar-kimberly-smith.webp',
+        avatarURL: 'https://i.ibb.co/2hv1CCg/avatar-kimberly-smith.webp',
         action: 'commented on your picture',
         time: '1 week ago',
         isRead: true,
-        objectImgURL: '/src/assets/images/image-chess.webp'
+        objectImgURL: 'https://i.ibb.co/Fqd74NM/image-chess.webp'
     }, {
         senderName: 'Nathan Peterson',
-        avatarURL: 'src/assets/images/avatar-nathan-peterson.webp',
+        avatarURL: 'https://i.ibb.co/HnTd2xd/avatar-nathan-peterson.webp',
         action: 'reacted to your recent post',
         objectName: '5 end-game strategies to increase your win rate',
         time: '2 weeks ago',
         isRead: true
     }, {
         senderName: 'Anna Kim ',
-        avatarURL: 'src/assets/images/avatar-anna-kim.webp',
+        avatarURL: 'https://i.ibb.co/WFkMdjX/avatar-anna-kim.webp',
         action: 'left the group',
         objectName: 'Chess Club',
         time: '2 weeks ago',
