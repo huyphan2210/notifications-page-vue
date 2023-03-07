4fe8ec2b93159c6cacc6ccfd175191ee36b9e86b
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-07 09:00:16 +0700

    Add structure + Add markRead handler + Finish Styling

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -1,20 +1,37 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { senders } from '../data/senders'
+import { Sender } from '../interfaces/Sender';
 import Activity from '../components/Activity.vue';
-const unread = ref(3);
+
+const people = ref([...senders])
+
+const unread = ref(people.value.filter(sender => !sender.isRead).length);
+
+const markRead = (sender: Sender) => {
+    sender.isRead = true;
+    unread.value = people.value.filter(sender => !sender.isRead).length;
+}
+
+const markAllRead = () => {
+    people.value.forEach(person => {
+        person.isRead = true;
+    })
+    unread.value = 0;
+}
+
 </script>
 
 <template>
     <div class="notifications">
         <div class="header">
             <hgroup>
-                <h1>Notifications <span>{{ unread }}</span></h1>
-                <span>Mark all as read</span>
+                <h1>Notifications <span v-if="unread > 0">{{ unread }}</span></h1>
+                <span @click="markAllRead">Mark all as read</span>
             </hgroup>
         </div>
         <div class="activities">
-            <Activity class="activity" :sender="sender" v-for="sender in senders"></Activity>
+            <Activity @click="markRead(sender)" :style="!sender.isRead ? `background-color: hsl(210, 60%, 98%);` : ''" class="activity" :sender="sender" v-for="sender in people"></Activity>
         </div>
     </div>
 </template>
@@ -22,7 +39,8 @@ const unread = ref(3);
 <style scoped>
 .notifications {
     background-color: white;
-    padding: 1rem;
+    padding: 1rem 2rem;
+    border-radius: 0.5rem;
 }
     hgroup {
         display: flex;
@@ -30,9 +48,47 @@ const unread = ref(3);
         align-items: center;
     }
 
+        hgroup h1 {
+            color: hsl(224, 21%, 14%);
+        }
+            hgroup h1 span {
+                display: inline-block;
+                font-size: 1.5rem;
+                padding: 0.25rem 0.5rem;
+                color: white;
+                background-color: hsl(219, 85%, 26%);
+                border-radius: 0.5rem;
+                font-style: normal;
+                text-decoration: none;
+            }
+                hgroup h1 span:hover {
+                    cursor: initial;
+                }
+
+        hgroup span {
+            color: hsl(219, 14%, 63%);
+            font-weight: 500;
+        }
+            hgroup span:nth-child(2):hover {
+                cursor: pointer;
+                color: hsl(219, 85%, 26%);
+            }
+
 .activity {
     margin-block-end: 1rem;
-    background-color: hsl(205, 33%, 90%);
+    border-radius: 0.5rem;
 }
+    .activity:hover {
+        cursor: pointer;
+        background-color: hsl(211, 68%, 94%) !important;
+    }
+@media screen and (max-aspect-ratio: 1) {
+    .notifications {
+        padding: 0 0.5rem;
+    }
 
+    hgroup h1 {
+        font-size: 1.5rem;
+    }
+}
 </style>
