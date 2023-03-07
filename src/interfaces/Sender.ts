f47eeade636ca3376781e8b5a28ac9d654824d8d
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-07 08:58:30 +0700

    Add objectColor to Sender interface

--- src/interfaces/Sender.ts
+++ src/interfaces/Sender.ts
@@ -3,6 +3,7 @@ interface Sender {
     avatarURL: string,
     action: string,
     objectName?: string,
+    objectColor?: string,
     time: string,
     objectImgURL?: string,
     message?: string,
